89212133f936036a06472714c7bf4213859767fa
JoseRodriM <josearm_1601@hotmail.com>
2022-02-22 16:47:46 +0100

    update javascript file

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log('Hola')
+let python = document.getElementById('python'),
+    enviar = document.querySelector('.enviar');
+
+console.log(python.checked)
+
+function display(){
+    if(python.checked){
+        return python.value
+    }else{
+        return 'No está'
+    }
+}
+
+enviar.addEventListener('click', (e) =>{
+    e.preventDefault()
+    console.log('hola')
+    console.log(display())
+});
